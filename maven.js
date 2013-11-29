2b42dd20f6c005e2be5a5e5076c1c4c7111af530
peterqliu <peterqliu@gmail.com>
2013-11-29 00:45:06 -0800

    push

--- maven.js
+++ maven.js
@@ -145,7 +145,7 @@ for (var i=0; i<niches.length; i++)
 	} 
 	
 var specialties= ['Investments', 'Financial Planning', 'Retirement Plans', 'Budget/Debt Management', '401(K)/403(B)/IRAS', 'Taxes And Accounting', 'Real Estate', 'Insurance', 'Estate Planning', 'Choosing An Advisor']
-for (var i=0; i<specialties.length; i++) console.log('specialty')
+for (var i=0; i<specialties.length; i++) 
 	{	$('#specialty').append('<span class="ck-button choices"><label><input type="checkbox" value="1"><span>'+specialties[i]+'</span></label></span>');
 	} 
 // Search page questionnaire
